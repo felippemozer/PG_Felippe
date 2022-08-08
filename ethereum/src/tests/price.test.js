d92204f16254c17b0c37a64bbdb246f3073fef48
felippemozer <felippemozer22@gmail.com>
2022-08-08 19:43:20 -0300

    ETH client changes

--- ethereum/src/tests/price.test.js
+++ ethereum/src/tests/price.test.js
@@ -1,15 +1,34 @@
-const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { data } = require('../data/feeds.json');
 
+const DIVISOR = 100000000;
+
+/**
+ * Get some random data feeds hash in the feeds.json file
+ * @returns Some of the data feeds hashes to be included in the contract
+ */
+function getRandomHash() {
+    const props = Object.getOwnPropertyNames(data.rinkeby.hashes);
+    const index = Math.floor(Math.random() * props.length);
+    const propName = props[index];
+    const someHash = data.rinkeby.hashes[propName];
+
+    return [propName, someHash];
+}
+
+const hash = getRandomHash();
 
 describe("PriceConsumer", function () {
   it("test PriceConsumer contract", async function () {
     const PriceConsumer = await ethers.getContractFactory("PriceConsumer");
-    const consumer = await PriceConsumer.deploy();
+    console.log(`Feed: ${hash[0]}\n`);
+    const consumer = await PriceConsumer.deploy(hash[1]);
+    const startTime = performance.now();
     await consumer.deployed();
+    const finishTime = performance.now();
     console.log('price consumer deployed at:'+ consumer.address);
-    // VERIFICAR O QUE TESTAR PARA CONFIRMAR O RESULTADO
-    expect(await consumer.getLatestPriceAndDescription().description.to.equal());
+    const result = await consumer.getLatestPriceAndDescription();
+    console.log(`\nPrice of ${result[1]} is ${parseFloat(result[0].toString())/DIVISOR}`);
+    console.log(`\nTime of execution: ${(finishTime - startTime)/1000} seconds\n`);
   });
 });
